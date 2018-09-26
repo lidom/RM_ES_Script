ce70440fd6f3d57d2929e6ab4d0541cd0cdf4d22
Dominik Liebl <liebl.dominik@gmail.com>
2018-09-26 17:31:13 +0200

    Remove lower link on navigation panel and added new chapter

--- build_bookdown_script.R
+++ build_bookdown_script.R
@@ -1,2 +1,2 @@
-bookdown::render_book("index.Rmd")
-bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+bookdown::render_book(input = "index.Rmd")
+bookdown::render_book(input = "index.Rmd", output_format = "bookdown::pdf_book")
---
+++ docs/02-Test-Theory.md
@@ -0,0 +1,3 @@
+# Test Theory
+
+Details on test theory.
